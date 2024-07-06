62ffd6e4cce8ce90dbc1707fdeca314eb4f7097a
Sachin Alam <sachinalam4216@gmail.com>
2024-07-06 16:15:17 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-green-600 p-4'>React Router</h1>
+    </>
+  )
+}
+
+export default App
